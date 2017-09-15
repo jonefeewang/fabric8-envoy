bfba73e9db569583cc68c110523ca74910b4f70c
jonefeewang <jonefeewang@gmail.com>
2017-09-15 11:25:41 +0800

    增加安装文件

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-yum -y update 
+
+mkdir -p ~/yum_bake/
+mv /etc/yum.repos.d/* ~/yum_bake/
+mv ../CentOS-Base.repo /etc/yum.repos.d/
+
+yum -y update
 yum -y groupinstall 'Development Tools'
 # make sure i dont miss any thing 
 yum install -y cmake wget gmp-devel mpfr-devel libmpc-devel golang clang c-ares-devel
@@ -7,12 +12,6 @@ yum install -y cmake wget gmp-devel mpfr-devel libmpc-devel golang clang c-ares-
 # we need latest version of the git for the build hence will remove the existing version
 yum -y remove  git 
 
-rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh https://centos7.iuscommunity.org/ius-release.rpm
-yum -y update 
-# disable base repo for installation as it has older git version
-yum -y --disablerepo=base,updates --enablerepo=ius install git
 
 # Directory that will have envoy tools
 mkdir -p /opt/envoy_env
-chown -R vagrant:vagrant /opt/envoy_env
